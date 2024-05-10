a1d71f67e913097f89e4f69348a402f75e0fe896
ygorp <ygor-nogueira@hotmail.com>
2024-05-10 09:51:03 -0300

    implementando o db postgres através do neon

---
+++ db.js
@@ -0,0 +1,21 @@
+import 'dotenv/config'; //process.env
+import postgres from "postgres";
+
+// Connect to the database
+
+const postgres = require('postgres');
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
